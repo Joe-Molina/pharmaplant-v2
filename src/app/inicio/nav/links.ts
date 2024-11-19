e37b9926acb1d075c9dfd23735dff88d1bd3b08b
jodomodev <joedodaniljr123ggmail.com>
2024-11-19 16:58:46 -0400

    first commit

---
+++ src/app/inicio/nav/links.ts
@@ -0,0 +1,14 @@
+export const links = [
+  {
+    link: '/productos',
+    text: 'Productos'
+  },
+  {
+    link: '/notisalud',
+    text: 'NotiSalud'
+  },
+  {
+    link: '/sobre_nosotros',
+    text: 'Sobre Nosotros'
+  },
+]
